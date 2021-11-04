1dc8b1ec352a4732d33d044ee26cd293d27e2048
André Kuhlmann <akuhltime@gmail.com>
2021-11-04 11:45:37 +0100

    Fix output.pdf Error Response

--- api/router/output.ts
+++ api/router/output.ts
@@ -10,53 +10,52 @@ import getLatestRelease from '../lib/getLatestRelease'
 const router = Router()
 
 router.get('/', async (_req, res) => {
-  try {
-    const latestRelease = await getLatestRelease(repo)
+  const latestRelease = await getLatestRelease(repo)
 
-    // In case there is no latest release the action is either still running
-    // or there was an error during compilation.
-    if (!latestRelease) {
-      const running = await isActionRunning(repo)
+  console.log(latestRelease)
 
-      if (running) {
-        res.sendFile(join(__dirname, '../../.assets/busy.pdf'))
-      } else if (!(await isLastGenerationSuccessfull(repo))) {
-        res.sendFile(join(__dirname, '../../.assets/error.pdf'))
-      }
+  // In case there is no latest release the action is either still running
+  // or there was an error during compilation.
+  if (!latestRelease) {
+    const running = await isActionRunning(repo)
 
-      return
+    if (running) {
+      res.sendFile(join(__dirname, '../../.assets/busy.pdf'))
+    } else {
+      res.sendFile(join(__dirname, '../../.assets/error.pdf'))
     }
 
-    const assetInformation = latestRelease.assets[0]
-
-    const assetId = assetInformation.id
-    const releaseTag = latestRelease.name?.replace('.', '_')
-
-    axios
-      .get(
-        `https://api.github.com/repos/${repo.owner}/${repo.repo}/releases/assets/${assetId}`,
-        {
-          responseType: 'stream',
-          headers: {
-            Accept: 'application/octet-stream',
-            Authorization: `Bearer ${env.GITHUB_TOKEN}`
-          }
-        }
-      )
-      .then(response => {
-        res.setHeader('Content-Type', 'application/pdf')
-        res.setHeader(
-          'Content-Disposition',
-          `attachment; filename=${releaseTag}.pdf`
-        )
-        response.data.pipe(res)
-      })
-  } catch (error: any) {
-    res.status(500).send({
-      message: 'No release Available at the moment',
-      error: error.message
-    })
+    return
   }
+
+  const assetInformation = latestRelease.assets[0]
+
+  const assetId = assetInformation.id
+  const releaseTag = latestRelease.name?.replace('.', '_')
+
+  axios
+    .get(
+      `https://api.github.com/repos/${repo.owner}/${repo.repo}/releases/assets/${assetId}`,
+      {
+        responseType: 'stream',
+        headers: {
+          Accept: 'application/octet-stream',
+          Authorization: `Bearer ${env.GITHUB_TOKEN}`
+        }
+      }
+    )
+    .then(response => {
+      res.setHeader('Content-Type', 'application/pdf')
+      res.setHeader(
+        'Content-Disposition',
+        `attachment; filename=${releaseTag}.pdf`
+      )
+      response.data.pipe(res)
+    })
+    .catch(error => {
+      console.error(error)
+      res.send('Error')
+    })
 })
 
 export default router
