c72fcb4cbb237cd4b153d9777c33f87022e2bde8
André Kuhlmann <akuhltime@gmail.com>
2021-11-04 11:48:30 +0100

    Update Info Route

--- api/router/info.ts
+++ api/router/info.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import repo from '../config/repo'
 import getLatestWorkflowRun from '../lib/getLatestWorkflowRun'
+import getLatestRelease from '@/lib/getLatestRelease'
 
 const router = Router()
 
@@ -8,10 +9,19 @@ router.get('/', async (req, res) => {
   const baseURL = req.protocol + '://' + req.get('host')
 
   const latestRun = await getLatestWorkflowRun(repo)
+  const latestRelease = await getLatestRelease(repo)
 
   res.send({
-    workflowStatus: latestRun?.status,
-    lastRun: latestRun?.created_at,
+    workflow: {
+      status: latestRun?.status || null,
+      timestamp: latestRun?.created_at || null
+    },
+    release: latestRelease
+      ? {
+          tag: latestRelease.tag_name,
+          timestamp: latestRelease.published_at
+        }
+      : null,
     links: {
       output: `${baseURL}/output.pdf`,
       shield: `${baseURL}/shield.svg`
