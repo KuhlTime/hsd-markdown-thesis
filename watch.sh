3a9212ff60493d7d0b869d044625290718e4d423
André Kuhlmann <akuhltime@gmail.com>
2022-04-01 09:40:51 +0000

    Auto open content.pdf on gitpod

--- watch.sh
+++ watch.sh
@@ -3,5 +3,8 @@
 # First run
 sh ./generate.sh
 
+# Only on gitpod
+whoami | grep 'gitpod' > /dev/null && code -r content.pdf
+
 # Listen for changes
 when-changed -1 -r content -c "sh ./generate.sh"
